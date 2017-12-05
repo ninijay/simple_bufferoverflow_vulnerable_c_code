88988a2b1572f4d63614250229488e64cb207290
root <ninijay93@gmail.com>
2017-12-05 21:18:46 +0100

    Added comments to code

--- bufferoverflow.c
+++ bufferoverflow.c
@@ -2,16 +2,22 @@
 #include<stdlib.h>
 int main()
 {
+    //Variable and Memory Allocation
     char *place;
     char *systemcommand;
-    place=(char *) malloc(10);
-    systemcommand=(char *) malloc(128);
-	printf("memory address of place is : %d\n", place);     
-    printf("memory address of systemcommand is : %d\n", systemcommand);
-    printf("The space in memory between place and systemcommand is : %d\n", systemcommand-place);
+    place=(char *)malloc(10);
+    systemcommand=(char *)malloc(128);
+    // Printing out memory information to user
+    printf("memory address of place is : %d\n", place);
+    printf("memory address of systemcommand is: %d\n", systemcommand);
+    printf("the space in memory betweenn place and systemcommand is :%d\n", systemcommand-place);
+    // Prompting user for best place to learn hacking
     printf("Where is the best place on the web to learn hacking?");
     gets(place);
+    // Printing answer to user
     printf("The best place to learn hacking on the web is %s\n", place);
+    // Executing command stored in systemcommand
     system(systemcommand);
+
     return 0;
 }
